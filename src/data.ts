7842ab2e442c53ba3426c6a4fe12befc0ddf7aaa
jeff-romero <jeffromero86@gmail.com>
2026-06-17 16:36:49 -0700

    add sample accounts data

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+import { Account } from './app/shared/models/account';
+
+export const sammpleAccounts: Account[] = [
+    {
+        accountNumber: 5555555555,
+        firstName: 'John',
+        lastName: 'Doe',
+        checkingBalance: 24563,
+        savingsBalance: 156798
+    },
+    {
+        accountNumber: 7429995109,
+        firstName: 'Jane',
+        middleName: 'Capo',
+        lastName: 'Smith',
+        checkingBalance: 60721,
+        savingsBalance: 821112
+    },
+    {
+        accountNumber: 1212121212,
+        firstName: 'Darius',
+        lastName: 'Noxus',
+        checkingBalance: 503,
+        savingsBalance: 2407
+    },
+    {
+        accountNumber: 8945613373,
+        firstName: 'Teemo',
+        lastName: 'Shrewm',
+        checkingBalance: 972
+    }
+];
