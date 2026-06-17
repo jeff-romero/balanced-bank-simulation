cbf49ccce8cfeafcf4dc330a72288523eb1aadc7
jeff-romero <jeffromero86@gmail.com>
2026-06-17 16:11:46 -0700

    add model for account

---
+++ src/app/shared/models/account.ts
@@ -0,0 +1,8 @@
+export class Account {
+    accountNumber!: number;
+    firstName!: string;
+    middleName?: string;
+    lastName!: string;
+    checkingBalance!: number;
+    savingsBalance?: number;
+}
