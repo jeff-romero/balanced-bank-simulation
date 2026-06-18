c1b72f8a6075c272c9a74bfbc449129293335a1c
jeff-romero <jeffromero86@gmail.com>
2026-06-18 16:53:47 -0700

    add home page and login page to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: App },
+    { path: './components/login', component: Login }
+];
